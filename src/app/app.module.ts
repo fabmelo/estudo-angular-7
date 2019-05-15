256a7f0ee7e627cbbead2b315bdae4d4f6b1856a
Fabiano Melo Moreira <fab_melo60@hotmail.com>
2019-05-15 15:52:07 -0300

    Implementado o módulo categories com seu master detail e seu devido roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 
 @NgModule({
---
+++ src/app/pages/categories/categories.module.ts
@@ -0,0 +1,19 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Modules
+import { CategoriesRoutingModule } from './categories-routing.module';
+
+// Components
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+
+@NgModule({
+  declarations: [CategoryListComponent, CategoryFormComponent],
+  imports: [
+    CommonModule,
+    CategoriesRoutingModule
+  ]
+})
+export class CategoriesModule { }
