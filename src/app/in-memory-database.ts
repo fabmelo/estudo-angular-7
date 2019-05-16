22a3ba1c0c51f7fdaffc7202909f2d501e2d7c11
Fabiano Melo Moreira <fab_melo60@hotmail.com>
2019-05-16 07:46:49 -0300

    Implementação do model de categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,9 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
-    const categories = [
+    const categories: Category[] = [
       { id: 1, name: 'Lazer', description: 'Mensalidade do Clube' },
       { id: 2, name: 'Saúde', description: 'Mensalidade do Plano de Saúde' },
       { id: 3, name: 'Educação', description: 'Mensalidade da Faculdade' },
